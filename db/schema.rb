375d75251683633ede1ff7e5c1d99c693f097abd
tetuiij <tetu0902501@gmail.com>
2020-05-22 00:41:13 +0900

    Add name

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200516174628) do
+ActiveRecord::Schema.define(version: 20200518144959) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "title"
     t.string   "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
 end
